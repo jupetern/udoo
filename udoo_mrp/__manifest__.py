0c5be7404281c259ef3a842e9cd8f0fcb336b72d
Peter Nguyen <willpetern@gmail.com>
2024-03-16 22:26:51 +0700

    [ADD] udoo_mrp: init modules

---
+++ udoo_mrp/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2023 Alphatech Co., Ltd (https://alpha-tech.asia)
+{
+    'name': 'Manufacturing',
+    'category': 'Manufacturing',
+    'version': '17.0.1.0',
+    'license': 'OPL-1',
+    'author': 'Jupetern',
+    'website': 'https://github.com/jupetern',
+    'depends': [
+        'mrp',
+    ],
+    'data': [
+        'views/bom_component_view.xml',
+        'views/workorder_views.xml',
+        'views/menuitems.xml',
+    ],
+    'demo': [],
+}
